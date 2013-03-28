afebd59f1040f4a75af16aa136328a8159035e55
Brice Durand <brice.durand2@gmail.com>
2013-03-28 17:00:54 +0100

    Display todo list using handlebars

--- labs/architecture-examples/meteor-mine/app.js
+++ labs/architecture-examples/meteor-mine/app.js
@@ -1,17 +1,26 @@
-//Todos = new Meteor.Collection("todos");
+Todos = new Meteor.Collection("todos");
 
 
-//if (Meteor.isClient) {
-//    Meteor.subscribe("todos");
-//}
-//
-//if (Meteor.isServer) {
-//    Meteor.startup(function () {
-//        // code to run on server at startup
-//
-//    });
-//
-//    Meteor.publish("todos", function () {
-//        return Todos.find();
-//    });
-//}
+if (Meteor.isClient) {
+    Meteor.subscribe("todos");
+
+    Template.todoapp.todoCount = function() {
+        return Todos.find().count();
+    };
+
+    Template.main.todos = function() {
+        return Todos.find();
+    };
+}
+
+if (Meteor.isServer) {
+    Meteor.startup(function () {
+        // code to run on server at startup
+
+
+    });
+
+    Meteor.publish("todos", function () {
+        return Todos.find();
+    });
+}
