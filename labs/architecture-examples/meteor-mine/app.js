d0954ed2de4e9e4c5c6e85d889cdce1f1036a7e5
Brice Durand <brice.durand2@gmail.com>
2013-03-28 16:36:54 +0100

    initial ocmmit on my version of meteor app

---
+++ labs/architecture-examples/meteor-mine/app.js
@@ -0,0 +1,17 @@
+//Todos = new Meteor.Collection("todos");
+
+
+//if (Meteor.isClient) {
+//    Meteor.subscribe("todos");
+//}
+//
+//if (Meteor.isServer) {
+//    Meteor.startup(function () {
+//        // code to run on server at startup
+//
+//    });
+//
+//    Meteor.publish("todos", function () {
+//        return Todos.find();
+//    });
+//}
