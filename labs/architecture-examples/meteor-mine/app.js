8aed188fe09f41617a1e1548025b7f29722dbf0f
Brice Durand <brice.durand2@gmail.com>
2013-03-28 18:36:37 +0100

    Add new todo on enter

--- labs/architecture-examples/meteor-mine/app.js
+++ labs/architecture-examples/meteor-mine/app.js
@@ -2,6 +2,8 @@ Todos = new Meteor.Collection("todos");
 
 
 if (Meteor.isClient) {
+    var ENTER_KEY = 13;
+
     Meteor.subscribe("todos");
 
     Template.todoapp.todoCount = function() {
@@ -11,6 +13,16 @@ if (Meteor.isClient) {
     Template.main.todos = function() {
         return Todos.find();
     };
+
+    Template.todoapp.events({
+        'keydown #new-todo': function (event) {
+            var todoTitle = event.target.value.trim();
+            if (event.keyCode == ENTER_KEY && todoTitle != "") {
+                Todos.insert({title: event.target.value, completed: false});
+                event.target.value = "";
+            }
+        }
+    });
 }
 
 if (Meteor.isServer) {
