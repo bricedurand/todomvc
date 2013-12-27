3b04cc5f529e3334e02b222565f542536d34a84c
Brice Durand <brice.durand2@gmail.com>
2013-12-27 22:35:44 +0100

    Add todo feature

--- labs/architecture-examples/angular-custom/js/app.js
+++ labs/architecture-examples/angular-custom/js/app.js
@@ -3,4 +3,11 @@ function TodoCtrl($scope) {
     {title: 'Create a TodoMVC template', completed: true},
     {title: 'Rule the web', completed: false}];
  
+  $scope.addTodo = function() {
+    if ($scope.todoText.length == 0)
+      return;
+
+    $scope.todos.push({ title: $scope.todoText.trim(), completed: false });
+    $scope.todoText = "";
+  }
 }
