29b50ba7ed1ab1291dfa27334270c9a0548190cf
Brice Durand <brice.durand2@gmail.com>
2024-08-21 10:50:19 +0200

    add Footer component with no interactions

--- examples/react-18/src/App.jsx
+++ examples/react-18/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header";
 import Main from "./Main";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div>
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
