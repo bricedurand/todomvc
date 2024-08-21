a8b3294fd8cd274824b978e1d7ca96890b745053
Brice Durand <brice.durand2@gmail.com>
2024-08-21 11:37:36 +0200

    replace list items with an array passed via props

--- examples/react-18/src/App.jsx
+++ examples/react-18/src/App.jsx
@@ -2,11 +2,13 @@ import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
 
+const todoList = [ { id: 1, text: "Kiffer" }, { id: 2, text: "Rire" } ]
+
 function App() {
   return (
     <div>
       <Header />
-      <Main />
+      <Main todoList={todoList} />
       <Footer />
     </div>
   );
--- examples/react-18/src/Main.jsx
+++ examples/react-18/src/Main.jsx
@@ -1,4 +1,17 @@
-export default function Main() {
+export default function Main({ todoList }) {
+  const listItems = todoList.map(todo => {
+    return(
+      <li key={todo.id}>
+        <div className="view">
+          <input className="toggle" type="checkbox" />
+          <label>{todo.text}</label>
+          <button className="destroy"></button>
+        </div>
+        <input className="edit" />
+      </li>
+    )
+  });
+
   return (
     // This section should be hidden by default and shown when there are todos
     <section className="main">
@@ -6,22 +19,7 @@ export default function Main() {
       <label htmlFor="toggle-all">Mark all as complete</label>
       <ul className="todo-list">
         {/* List items should get the class `editing` when editing and `completed` when marked as completed */}
-        <li className="completed">
-          <div className="view">
-            <input className="toggle" type="checkbox" defaultChecked />
-            <label>Kiffer</label>
-            <button className="destroy"></button>
-          </div>
-          <input className="edit" />
-        </li>
-        <li>
-          <div className="view">
-            <input className="toggle" type="checkbox" />
-            <label>Rire</label>
-            <button className="destroy"></button>
-          </div>
-          <input className="edit" />
-        </li>
+        {listItems}
       </ul>
     </section>
   );
