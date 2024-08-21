3ff1dc36b4a8d9b1d3b70b26f6c2d222b75fc955
Brice Durand <brice.durand2@gmail.com>
2024-08-21 11:54:57 +0200

    hide main section if no todos using inline styling

--- examples/react-18/src/Main.jsx
+++ examples/react-18/src/Main.jsx
@@ -14,7 +14,7 @@ export default function Main({ todoList }) {
 
   return (
     // This section should be hidden by default and shown when there are todos
-    <section className="main">
+    <section className="main" style={{display: todoList.length == 0 && 'none'}}>
       <input id="toggle-all" className="toggle-all" type="checkbox" />
       <label htmlFor="toggle-all">Mark all as complete</label>
       <ul className="todo-list">
