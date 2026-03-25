2208c8b2e7ba753f6a4af42a4d84db6a64826219
Brice Durand <brice.durand2@gmail.com>
2026-03-25 10:29:15 +0100

    extract item list in separate component

---
+++ examples/vue-3/src/components/TodoItem.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  props: {
+    text: String
+  }
+}
+</script>
+
+<template>
+    <!-- List items should get the class `editing` when editing and `completed` when marked as completed -->
+    <li class="completed">
+        <div class="view">
+        <input class="toggle" type="checkbox" checked>
+        <label>{{ text }}</label>
+        <button class="destroy"></button>
+        </div>
+        <input class="edit" value="{{ text }}"">
+    </li>
+</template>
