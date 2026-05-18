1902532d703e6e6690d8cf3e730289f2122581bd
Brice Durand <brice.durand2@gmail.com>
2026-05-18 16:46:45 +0200

    load assets in main js file and remove static copy of base js

--- examples/vue/src/main.js
+++ examples/vue/src/main.js
@@ -1,3 +1,8 @@
+import './assets/main.css'
+import 'todomvc-app-css/index.css'
+import 'todomvc-common/base.css'
+import 'todomvc-common/base.js'
+
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
